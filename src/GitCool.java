afcf7f7fd14a84b27c6d5c8a08c3b9eeca1ece67
Yasinince <incey1907@gmail.com>
2021-07-21 13:21:26 -0400

    i am done with more work ready to push

--- src/GitCool.java
+++ src/GitCool.java
@@ -5,4 +5,11 @@ public class GitCool {
 
     // We have added our GitHub account under intelliJ
     // Make sure to click apply and click on to save
+
+    // we were able to successfully shared project under our github account
+    // Share Project On github happen only one time per project
+    // it combines the 3 actions below
+    // Create remote repository under your account
+    // Create the link between local repo to remote repo
+    // Push local commits to remote repo
 }
