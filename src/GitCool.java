dad8c6f7d94dcf13aad8aa94782db740b2ffbaf0
Yasinince <incey1907@gmail.com>
2021-07-21 13:18:51 -0400

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,7 @@ public class GitCool {
     //B23 IS LEARNING GIT
     //B23 IS A SPECIAL NUMCER
     //B23 IS MAKING SOME SENSE NOW
+
+    // We have added our GitHub account under intelliJ
+    // Make sure to click apply and click on to save
 }
